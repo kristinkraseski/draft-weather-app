329361eac330eee61319636baf0e876e04eb938d
kristinkraseski <89749013+kristinkraseski@users.noreply.github.com>
2021-09-12 12:37:14 -0400

    Linked humidity and wind speed to real data when city is searched

--- src/script.js
+++ src/script.js
@@ -47,9 +47,13 @@ function displayTemperature(response) {
   let cityEntered = response.data.name;
   let temperature = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#current-description");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#current-wind");
   showCityName.innerHTML = `${cityEntered}`;
   showTemp.innerHTML = `${temperature}`;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 function searchCity(city) {
