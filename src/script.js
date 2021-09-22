f7b53c8d7d5db1690feaab091b6744d2635e5626
kristinkraseski <89749013+kristinkraseski@users.noreply.github.com>
2021-09-22 15:50:05 -0400

    Changed spacing of forecast in CSS

--- src/script.js
+++ src/script.js
@@ -46,7 +46,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
